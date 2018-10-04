88f52c3e27d59691e5c8de94c133c50bd9f0a190
rochdi1 <rochdi.abdou3@gmail.com>
2018-10-04 13:39:28 +0200

    promise avancee

--- promise.js
+++ promise.js
@@ -11,8 +11,44 @@ const p = new Promise((resolve, reject) => {
 
 });
 
-
 //then donne le retour favorable et le catch donne le cas defavorable
 p.then((user)=>console.log('resolve : ', user))
  .catch((err)=>console.log('reject : ', err.message));
 
+ console.log('before');
+    getUsers(2).then((user) => {
+                                console.log(user);
+                                return getRepos(user.id);
+                                })
+               .then((repos) => {
+                    console.log(repos);
+                });
+
+console.log('after');
+
+ function getUsers(id){
+
+    return new Promise((resolve, reject) => {
+
+          setTimeout(() => {
+                                console.log('get user from database.....');
+                                resolve({
+                                    id: id,
+                                    gitHubusername: "rochdi Abdelatif"
+                                })
+                            }, 2000);
+    });
+  
+}
+
+function getRepos(idUser){
+
+    return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                                console.log('get a repos.....');
+                                resolve(['repo1', 'repo2', 'repo3'])
+                            }, 2000);
+
+    });
+
+}

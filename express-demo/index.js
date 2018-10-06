bf42c94583c6b325b7264d4c4d7d0d663cfe3fbf
rochdi1 <rochdi.abdou3@gmail.com>
2018-10-06 16:35:30 +0200

    creation un API RestFull get zb: /api/courses

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 // associe un variable a express librarie 
 const app = express();
 
+//definir le variable courses 
+courses = ['Laravel', 'Angular', 'NodeJs', 'VueJs'];
+
+// cree API 
+app.get('/api/courses', (req, res) => {
+    // envoyer la reponse ce genre un tableau []
+    res.send(courses)
+});
 
 // cree un serveur
 // si un port configure en en .env le serveur prend le env Port si non prend 3000

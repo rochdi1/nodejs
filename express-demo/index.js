c1542f42412be70b887904fee50792f5a82762df
rochdi1 <rochdi.abdou3@gmail.com>
2018-10-06 16:20:03 +0200

    installtion nodemon

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,4 +5,6 @@ const app = express();
 
 
 // cree un serveur
-app.listen(3000, () => { console.log('app runing on 3000 port'); });
+// si un port configure en en .env le serveur prend le env Port si non prend 3000
+const PORT = process.env.PORT  || 3000;
+app.listen(PORT, () => { console.log(`app runing on ${ PORT } port`); });
